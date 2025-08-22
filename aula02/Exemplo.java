36053cf2c5bdfdaf88987c1dd3f77b93208ef786
amignon <amignon@gmail.com>
2025-08-22 01:31:09 +0000

    Aula 02 - Exemplo

---
+++ aula02/Exemplo.java
@@ -0,0 +1,8 @@
+public class Exemplo {
+    public static void main(String[] args) {
+        System.out.printf("%-16s", "Livro: ");
+        System.out.printf("Java para iniciantes\n");
+        System.out.printf("Dias em atraso: ");
+        System.out.printf("%d\n", 10);
+    }
+}
