265d45ba5b15535d69893b26970976711b1d1257
amignon <amignon@gmail.com>
2025-10-10 00:15:20 +0000

    Exemplo - Aula 07

---
+++ aula07/Aluno.java
@@ -0,0 +1,12 @@
+public class Aluno {
+    // atributos
+    private String nome;
+    private double nota1;
+    private double nota2;
+
+    // construtor
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+    
+}
---
+++ aula07/TestaAluno.java
@@ -0,0 +1,7 @@
+public class TestaAluno {
+    public static void main(String[] args) {
+        Aluno a = new Aluno();
+        a.nome
+    }
+    
+}
